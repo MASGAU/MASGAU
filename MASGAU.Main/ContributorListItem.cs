210ad058002cad570753f509fcd035fa76402a17
sanmadjack <sanmadjack@masgau.org>
2012-03-01 13:39:22 -0600

    Fix for MASGAU not reading new contributor tag

---
+++ MASGAU.Main/ContributorListItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MASGAU.Main
+{
+    class ContributorListItem {
+        public string name{ get; set; }
+        public string count{ get; set; }
+    }
+}
