5e45f08626232a1fdbf72a4d8ad29b44af57511f
Matthew Barbour <sanmadjack@masgau.org>
2012-05-02 16:55:22 -0500

    Added logger project and class

---
+++ Logger/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace Logger
+{
+    public class Logger
+    {
+
+        private static StreamWriter log_writer;
+
+
+        static Logger()
+        {
+            log_writer = new StreamWriter("log.txt", true, Encoding.UTF8);
+        }
+
+        public static void log(String line)
+        {
+            StringBuilder output = new StringBuilder(DateTime.Now.ToString());
+            output.Append(" - ");
+            output.Append(line);
+
+            log_writer.WriteLine(output.ToString());
+        }
+
+    }
+}
