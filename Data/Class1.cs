635a2b7cf100b43b2c2d93b45f84aa499420ba64
Matthew Barbour <sanmadjack@gmail.com>
2012-08-08 08:54:32 -0500

    Removed Data and Translations projects, moving submodules into MASGAU

--- Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿// This is just here so the compiler won't balk about there not being something to compile
-
-namespace Data
-{
-    public class Class1
-    {
-    }
-}
--- Translations/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿// This is just here so the compiler won't balk about there not being something to compile
-
-namespace Translations
-{
-    public class Class1
-    {
-    }
-}
