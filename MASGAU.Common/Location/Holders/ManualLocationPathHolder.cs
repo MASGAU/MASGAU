53e4acde5b22085b3b0369c3b7c3f4217c5bad7a
Matthew Barbour <sanmadjack@gmail.com>
2012-08-20 14:42:36 -0500

    Fixed commonappdata entries making duplicates and manual paths causing a null exception

--- MASGAU.Common/Location/Holders/ManualLocationPathHolder.cs
+++ MASGAU.Common/Location/Holders/ManualLocationPathHolder.cs
@@ -1,15 +1,15 @@
 ﻿using GameSaveInfo;
 namespace MASGAU.Location.Holders {
     public class ManualLocationPathHolder : LocationPath {
-        string manual_path;
+        public string ManualPath { get; protected set; }
 
 
         public ManualLocationPathHolder(string manual_path) {
-            this.manual_path = manual_path;
+            this.ManualPath = manual_path;
             this.override_virtual_store = true;
         }
         public override string ToString() {
-            return manual_path;
+            return ManualPath;
         }
     }
 }
