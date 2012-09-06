d4ce21a1f1e68c153ea0b17086d66652ee170db1
Matthew Barbour <sanmadjack@gmail.com>
2012-09-06 11:46:04 -0500

    Added ElevationResult for more detailed elevation feedback

---
+++ MASGAU.Common/ElevationResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MASGAU {
+    public enum ElevationResult {
+        Cancelled,
+        Failed,
+        Success
+    }
+}
