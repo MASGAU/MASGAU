b1c01f476479da68849e86d74a87cce9d7819481
Matthew Barbour <sanmadjack@gmail.com>
2012-09-13 13:43:50 -0500

    Started adding linker for #35

--- MASGAU.Common/ASymLinker.cs
+++ MASGAU.Common/ASymLinker.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Diagnostics;
+using System.IO;
 namespace MASGAU {
     public class ASymLinker {
+        protected Process linker;
+        protected ProcessStartInfo start_info = new ProcessStartInfo();
 
+        public static bool CanLink {
+            get {
+                return Common.AllUsersMode;
+            }
+        }
+
+        protected ASymLinker() {
+            linker = new Process();
+            linker.StartInfo = start_info;
+
+        }
+        public bool EstablishLink(string link_location, string target_location) {
+            DirectoryInfo Target = new DirectoryInfo(target_location);
+            DirectoryInfo Source = new DirectoryInfo(link_location);
+
+            if (Target.Exists) {
+                if (IsSymLink(link_location)) {
+                    // Already linked
+                } else {
+                    // Need to merge?
+                }
+            } else {
+                if (IsSymLink(link_location)) {
+                    // What the hell is it linked to?
+                } else {
+                    Source.MoveTo(target_location);
+
+                    // Needs to copy to destination
+                }
+            }
+
+            return true;
+        }
+
+        protected abstract bool IsSymLink(string location);
+        protected abstract bool CreateSymLink(string link_location, string target_location);
 
     }
 }
--- MASGAU.Windows/SymLinker.cs
+++ MASGAU.Windows/SymLinker.cs
@@ -2,9 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Diagnostics;
 namespace MASGAU {
-    public class SymLinker: ASymLinker {
+    public class SymLinker : ASymLinker {
 
+        private const string CheckArgs = "";
+        private const string CreateArgs = "";
+        private const string DeleteArgs = "";
+
+        public SymLinker() {
+        }
+
+
+
+        protected override bool CreateSymLink(string link_location, string target_location) {
+            throw new NotImplementedException();
+        }
+
+        protected override bool IsSymLink(string location) {
+            throw new NotImplementedException();
+        }
     }
 }
